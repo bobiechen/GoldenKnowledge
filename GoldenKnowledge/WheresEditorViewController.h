6f80b70f3bf0f31dc363f6209cb36900282a4c46
Bobie <bobie@herxun.co>
2013-01-28 11:15:39 +0800

    [what] add where's editor view-controller

---
+++ GoldenKnowledge/WheresEditorViewController.h
@@ -0,0 +1,13 @@
+//
+//  WheresEditorViewController.h
+//  GoldenKnowledge
+//
+//  Created by Bobie on 1/28/13.
+//  Copyright (c) 2013 BobieAir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WheresEditorViewController : UIViewController
+
+@end
---
+++ GoldenKnowledge/WheresEditorViewController.m
@@ -0,0 +1,38 @@
+//
+//  WheresEditorViewController.m
+//  GoldenKnowledge
+//
+//  Created by Bobie on 1/28/13.
+//  Copyright (c) 2013 BobieAir. All rights reserved.
+//
+
+#import "WheresEditorViewController.h"
+
+@interface WheresEditorViewController ()
+
+@end
+
+@implementation WheresEditorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
